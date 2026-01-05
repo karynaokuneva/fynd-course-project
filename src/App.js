44ed6c1ad145b24434a0f757430e76e66fdf9f3c
Karyna Okuneva <karynaokuneva@gmail.com>
2026-01-05 16:21:52 +0100

    create style file and folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import Counter from "./components/counter";
 import ClassCounter from "./components/ClassCounter";
 function App() {
   const [value, setValue] = useState("text in input");
 
   return (
     <div className="App">
-      <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>JavaScript</strong>
+          <div>
+            <p>JS - programming language</p>
+          </div>
+        </div>
+      </div>
+      <div className="post__btns">
+        <button>Delete</button>
+      </div>
     </div>
   );
 }
--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -5,6 +5,8 @@ class ClassCounter extends React.Component {
     this.state = {
       count: 0,
     };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
   increment() {
@@ -16,7 +18,7 @@ class ClassCounter extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.count}</h1>
+        <h1>{this.state.count}</h1>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
       </div>
